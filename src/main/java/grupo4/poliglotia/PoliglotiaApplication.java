8e27106ab09a79dd4b319184d84e36d815ec4fcc
Kennet Rodriguez <kennet_rl@hotmail.com>
2025-11-30 19:45:20 -0500

    feat: inicia proyecto Spring Boot

---
+++ src/main/java/grupo4/poliglotia/PoliglotiaApplication.java
@@ -0,0 +1,13 @@
+package grupo4.poliglotia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoliglotiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoliglotiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/grupo4/poliglotia/PoliglotiaApplicationTests.java
@@ -0,0 +1,13 @@
+package grupo4.poliglotia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoliglotiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
